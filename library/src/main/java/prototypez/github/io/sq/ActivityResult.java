3063b6d8d897d1a2391e5e73162cc6e73791a86a
prototypez <prototypez@126.com>
2018-08-02 11:42:29 +0800

    fix(library): avoid ActivityResult null data

--- library/src/main/java/prototypez/github/io/sq/ActivityResult.java
+++ library/src/main/java/prototypez/github/io/sq/ActivityResult.java
@@ -18,7 +18,6 @@ public class ActivityResult {
     public ActivityResult(int requestCode, int resultCode, Intent data, Bundle requestData, Bundle requestContextData) {
         this.requestCode = requestCode;
         this.resultCode = resultCode;
-        this.data = data;
 
         if (requestData != null || requestContextData != null) {
             if (requestData == null) {
@@ -30,6 +29,7 @@ public class ActivityResult {
             requestData.putBundle(SqConstant.KEY_REQUEST_CONTEXT_DATA, requestContextData);
             data.putExtra(SqConstant.KEY_REQUEST_DATA, requestData);
         }
+        this.data = data;
     }
 
     public int getRequestCode() {
